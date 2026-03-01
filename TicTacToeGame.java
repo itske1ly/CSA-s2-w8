a83ee9af29545f6752e140c8b6b7478129bc7760
itske1ly <davitske1ly@gmail.com>
2026-02-28 17:18:40 -0700

    tic

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,17 +1,17 @@
-public class TicTacToeGameBlank {
+public class TicTacToeGame{
 
     // Step 0: Initialize a 3X3 2D array named "board" 
+    String[][] board = new String[3][3];
     // TODO
-
     // This attribute is created for storing the current player
     // it will alternate between "X" and "O"
     private String currentPlayer;
 
     // Constructor: Call to start the game
-    public TicTacToeGameBlank() {
+    public TicTacToeGame() {
         // When game first starts, currentPlayer is "X"
         currentPlayer = "X";
-
+        System.out.println();
         // reset the board (clear all fields)
         resetBoard();
     }
@@ -19,6 +19,11 @@ public class TicTacToeGameBlank {
     // Step 1: Use nested loops to go through the whole array
     // and fill each position with " " (a single space)
     public void resetBoard() {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length;j++){
+                board[i][j] = " ";
+            }
+        }
         // TODO
     }
 
@@ -41,8 +46,9 @@ public class TicTacToeGameBlank {
     }
 
     // Step 2: Make a getter method that returns the current player
-    public ______ getCurrentPlayer() {
+    public String getCurrentPlayer() {
         //TODO
+            return currentPlayer;
     }
 
 
@@ -50,6 +56,12 @@ public class TicTacToeGameBlank {
     // This method will use "getCurrentPlayer" to check who the currently player is
     // and then set it to be the other person ("X" or "O")
     public void switchPlayer() {
+        if(currentPlayer.equals("X")){
+            currentPlayer = "O";
+        }
+        else{
+            currentPlayer = "X";
+        }
         // TODO
     }
 
@@ -58,14 +70,23 @@ public class TicTacToeGameBlank {
     // is in bounds. It returns true or false
     // For example, if the user's input numbers are (4,5), it returns false
     public boolean isInBounds(int row, int col) {
-        // TODO
-        return false;
+        if(row < 0 || row > 2){
+            return false;
+        }
+        if(col < 0 || col >2){
+
+            return false;
+        }
+            return true;
     }
 
 
     // Step 5: Make a method that checks if the location (row, col)
     // is empty (if it is " "). It returns true of false.
     public boolean isCellEmpty(int row, int col) {
+        if (board[row][col].equals(" ")){
+            return true;
+        }
         // TODO
         return false;
     }
@@ -78,6 +99,14 @@ public class TicTacToeGameBlank {
     // This method returns true if the mark is placed successfully, 
     // and false if the move cannot be made.
     public boolean placeMark(int row, int col) {
+        boolean cellEmpty = isCellEmpty(row, col);
+        boolean isInBound = isInBounds(row, col);
+        if(cellEmpty && isInBound){
+            board[row][col] = currentPlayer;
+            return true;
+
+        }
+
         // TODO
         return false;
     }
@@ -87,34 +116,65 @@ public class TicTacToeGameBlank {
     // Step 7: Make a method that checks whether the current player has won
     // you should check 3 rows/cols/diagonals using the last three helper functions
     public boolean checkWin() {
-        // TODO
+        if (checkRows() || checkCols() || checkDiagonals()) {
+        return true;
+        }
         return false;
-    }
+
+        }
 
     // Step 8: Make a method that checks for tie.
     // The conditions for a tie is there's not empty spot, AND 
     // nobody wins.
     public boolean checkTie() {
-        // TODO
-        return false;
+        
+        if(checkWin() == true){
+            return false;
+        }
+
+        for( int i = 0; i < 3; i++){
+            for( int j = 0; j < 3; j++){
+                if(isCellEmpty(i, j) == true){
+                    return false;
+
+                }
+                
+            }
+        }
+
+        return true;
     }
 
 //---------------------------------------------------------------------------
     // Helper Functions
     //returns true if any row is all the same mark (currentPlayer)
     public boolean checkRows() {
+
+        for(int i = 0; i < board.length; i++){
+           if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2]) && board[i][2].equals(currentPlayer)){
+            return true;
+           }
+        }
         // TODO
         return false;
     }
 
     //returns true if any column is all the same mark (currentPlayer)
     public boolean checkCols() {
+        for(int i = 0; i < board.length; i++){
+           if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i]) && board[2][i].equals(currentPlayer)){
+            return true;
+           }
+        }
         // TODO
         return false;
     }
 
     // returns true if either diagonal is all the same mark (currentPlayer)
     public boolean checkDiagonals() {
+             if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && board[2][2].equals(currentPlayer) || board[2][0].equals(board[1][1]) && board[1][1].equals(board[0][2]) && board[0][2].equals(currentPlayer)){
+            return true;
+           }
         // TODO
         return false;
     }
